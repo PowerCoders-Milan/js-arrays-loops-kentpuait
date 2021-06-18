ee629f465fd7512fe40cb6e50d8bd6e14c75e97e
kentpuait <kentpuait@1F-17.edu-il.it>
2021-06-18 12:42:13 +0200

    testo

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,14 @@
 // read carefully the instructions
+const myString = 'HELLO'
+var splits = myString.split();
+
+let stringArray = ['H', 'E', 'L', 'L', 'O']
+console.log(splits);
+
+const reversed = stringArray.reverse();
+console.log('reversed:', reversed);
+
+console.log(stringArray.join());
+// "HELLO"
+
+console.log(myString)
