d8eaf4df9cece47bba5449b7bbbd4262659f2e45
kentpuait <kentpuait@1F-17.edu-il.it>
2021-06-21 15:19:20 +0200

    exercise 2 solved

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -4,5 +4,5 @@ let stringArray = myString.split('');
 console.log(stringArray);
 stringArray.reverse();
 console.log(stringArray);
-console.log(stringArray.join(''));
-
+console.log(stringArray.join(' '));
+let reversedString = stringArray.join(' ');
--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,9 @@
 // function declaration
 const nMultiplesOf = (number, limit) => {
-
+    let multiples = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+    for (i = 0; i < limit.length; i++);
+    //console.log(multiplesOf)
+    return multiples;
 }
-
-console.log(nMultiplesOf(2, 10))
+//while (i < 20);
+console.log(multiples());
